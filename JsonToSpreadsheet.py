273e2d197658f6b8b757d97a23459934b1491c6f
PinkDraconian <robbedoes7000@hotmail.com>
2019-09-15 12:15:23 +0100

    Initial Commit

---
+++ JsonToSpreadsheet.py
@@ -0,0 +1,252 @@
+import xlsxwriter
+import json
+from os import listdir
+from os.path import isfile, join
+
+
+# Convert number to column number
+def column_string(n):
+    string = ""
+    while n > 0:
+        n, remainder = divmod(n - 1, 26)
+        string = chr(65 + remainder) + string
+    return string
+
+
+# Colors
+light_magenta = "#FF80FF"
+light_pink = "#FDDCE8"
+
+# Player UUID to name dict
+player_uuid_to_name_dict = {
+    '3b8b537d-4fe4-4038-897d-1ce0ce4cb894': 'Chabeloin',
+    '3be7c9f5-c656-4df2-af65-37f4f2f862b4': 'PinkDraconian',
+    '531b71f3-1b9a-4102-8148-692d575ab64c': 'BlazinPsycho420',
+    '6a7e1f3b-d1c4-444c-bed4-265d152349c5': 'mariejuliee',
+    '7efc6a1c-fd0b-46d2-8539-8db5dd7f159c': 'FriendlyBot',
+    '8c4ecf3b-13c6-45e9-8c91-c3122722568d': 'Wooselini',
+    'a9930b7e-accf-44fb-9b73-01f170064eb8': 'zoezoe_shpa',
+    'da78f155-c564-44b4-9faf-1f7731edcd51': 'greco1999',
+}
+
+# Get name of all stats files
+path = "D:\\Users\\vanro\\Documenten\\Data\\MinecraftBackups\\stats"
+players = [f for f in listdir(path) if isfile(join(path, f)) and f[:-5:] in player_uuid_to_name_dict.keys()]
+
+# Get playerdata
+playerdata = []
+for player in players:
+    file = open(path + "\\" + player, "r")
+    playerdata.append([player[:-5:], json.loads(file.readline())])
+
+# Get first rows
+killed_by_first_row = []
+crafted_first_row = []
+mined_first_row = []
+used_first_row = []
+killed_first_row = []
+dropped_first_row = []
+picked_up_first_row = []
+custom_first_row = []
+broken_first_row = []
+for statistics in playerdata:
+    stats = statistics[1]["stats"]
+    if "minecraft:killed_by" in stats:
+        for stat in stats["minecraft:killed_by"]:
+            if stat not in killed_by_first_row:
+                killed_by_first_row.append(stat)
+    if "minecraft:crafted" in stats:
+        for stat in stats["minecraft:crafted"]:
+            if stat not in crafted_first_row:
+                crafted_first_row.append(stat)
+    if "minecraft:mined" in stats:
+        for stat in stats["minecraft:mined"]:
+            if stat not in mined_first_row:
+                mined_first_row.append(stat)
+    if "minecraft:used" in stats:
+        for stat in stats["minecraft:used"]:
+            if stat not in used_first_row:
+                used_first_row.append(stat)
+    if "minecraft:killed" in stats:
+        for stat in stats["minecraft:killed"]:
+            if stat not in killed_first_row:
+                killed_first_row.append(stat)
+    if "minecraft:dropped" in stats:
+        for stat in stats["minecraft:dropped"]:
+            if stat not in dropped_first_row:
+                dropped_first_row.append(stat)
+    if "minecraft:picked_up" in stats:
+        for stat in stats["minecraft:picked_up"]:
+            if stat not in picked_up_first_row:
+                picked_up_first_row.append(stat)
+    if "minecraft:custom" in stats:
+        for stat in stats["minecraft:custom"]:
+            if stat not in custom_first_row:
+                custom_first_row.append(stat)
+    if "minecraft:broken" in stats:
+        for stat in stats["minecraft:broken"]:
+            if stat not in broken_first_row:
+                broken_first_row.append(stat)
+
+# Create a workbook
+
+workbook = xlsxwriter.Workbook('statistics.xlsx')
+
+# Formats
+cell_light_magenta = workbook.add_format({"bg_color": light_magenta, "bold": True})
+cell_light_pink = workbook.add_format({"bg_color": light_pink})
+cell_first_row = workbook.add_format({"bold": True, "right": 2})
+cell_first_column = workbook.add_format({"bold": True, "bottom": 2})
+cell_stat = workbook.add_format({"right": 3, "bottom": 3})
+
+#  Add worksheets and first rows.
+worksheets = []
+killed_by_worksheet = workbook.add_worksheet("Killed by")
+worksheets.append(killed_by_worksheet)
+for i in range(0, len(killed_by_first_row)):
+    killed_by_worksheet.write(0, i + 1, killed_by_first_row[i][10::].replace("_", " ").title(), cell_first_column)
+crafted_worksheet = workbook.add_worksheet("Crafted")
+worksheets.append(crafted_worksheet)
+for i in range(0, len(crafted_first_row)):
+    crafted_worksheet.write(0, i + 1, crafted_first_row[i][10::].replace("_", " ").title(), cell_first_column)
+mined_worksheet = workbook.add_worksheet("Mined")
+worksheets.append(mined_worksheet)
+for i in range(0, len(mined_first_row)):
+    mined_worksheet.write(0, i + 1, mined_first_row[i][10::].replace("_", " ").title(), cell_first_column)
+used_worksheet = workbook.add_worksheet("Used")
+worksheets.append(used_worksheet)
+for i in range(0, len(used_first_row)):
+    used_worksheet.write(0, i + 1, used_first_row[i][10::].replace("_", " ").title(), cell_first_column)
+killed_worksheet = workbook.add_worksheet("Killed")
+worksheets.append(killed_worksheet)
+for i in range(0, len(killed_first_row)):
+    killed_worksheet.write(0, i + 1, killed_first_row[i][10::].replace("_", " ").title(), cell_first_column)
+dropped_worksheet = workbook.add_worksheet("Dropped")
+worksheets.append(dropped_worksheet)
+for i in range(0, len(dropped_first_row)):
+    dropped_worksheet.write(0, i + 1, dropped_first_row[i][10::].replace("_", " ").title(), cell_first_column)
+picked_up_worksheet = workbook.add_worksheet("Picked up")
+worksheets.append(picked_up_worksheet)
+for i in range(0, len(picked_up_first_row)):
+    picked_up_worksheet.write(0, i + 1, picked_up_first_row[i][10::].replace("_", " ").title(), cell_first_column)
+custom_worksheet = workbook.add_worksheet("Custom")
+worksheets.append(custom_worksheet)
+for i in range(0, len(custom_first_row)):
+    custom_worksheet.write(0, i + 1, custom_first_row[i][10::].replace("_", " ").title(), cell_first_column)
+broken_worksheet = workbook.add_worksheet("Broken")
+worksheets.append(broken_worksheet)
+for i in range(0, len(broken_first_row)):
+    broken_worksheet.write(0, i + 1, broken_first_row[i][10::].replace("_", " ").title(), cell_first_column)
+
+# Filling up the spreadsheet
+row = 1
+for data in playerdata:
+    # Add name to each spreadsheet
+    for sheet in worksheets:
+        sheet.write(row, 0, player_uuid_to_name_dict.get(data[0]), cell_first_row)
+
+    # Add statistics
+    stats = data[1]["stats"]
+    for i in range(0, len(killed_by_first_row)):
+        killed_by_worksheet.write_blank(row, i + 1, "", cell_stat)
+        if "minecraft:killed_by" in stats:
+            if killed_by_first_row[i] in stats["minecraft:killed_by"]:
+                killed_by_worksheet.write(row, i + 1, stats["minecraft:killed_by"][killed_by_first_row[i]], cell_stat)
+    for i in range(0, len(crafted_first_row)):
+        crafted_worksheet.write_blank(row, i + 1, "", cell_stat)
+        if "minecraft:crafted" in stats:
+            if crafted_first_row[i] in stats["minecraft:crafted"]:
+                crafted_worksheet.write(row, i + 1, stats["minecraft:crafted"][crafted_first_row[i]], cell_stat)
+    for i in range(0, len(mined_first_row)):
+        mined_worksheet.write_blank(row, i + 1, "", cell_stat)
+        if "minecraft:mined" in stats:
+            if mined_first_row[i] in stats["minecraft:mined"]:
+                mined_worksheet.write(row, i + 1, stats["minecraft:mined"][mined_first_row[i]], cell_stat)
+    for i in range(0, len(used_first_row)):
+        used_worksheet.write_blank(row, i + 1, "", cell_stat)
+        if "minecraft:used" in stats:
+            if used_first_row[i] in stats["minecraft:used"]:
+                used_worksheet.write(row, i + 1, stats["minecraft:used"][used_first_row[i]], cell_stat)
+    for i in range(0, len(killed_first_row)):
+        killed_worksheet.write_blank(row, i + 1, "", cell_stat)
+        if "minecraft:killed" in stats:
+            if killed_first_row[i] in stats["minecraft:killed"]:
+                killed_worksheet.write(row, i + 1, stats["minecraft:killed"][killed_first_row[i]], cell_stat)
+    for i in range(0, len(dropped_first_row)):
+        dropped_worksheet.write_blank(row, i + 1, "", cell_stat)
+        if "minecraft:dropped" in stats:
+            if dropped_first_row[i] in stats["minecraft:dropped"]:
+                dropped_worksheet.write(row, i + 1, stats["minecraft:dropped"][dropped_first_row[i]], cell_stat)
+    for i in range(0, len(picked_up_first_row)):
+        picked_up_worksheet.write_blank(row, i + 1, "", cell_stat)
+        if "minecraft:picked_up" in stats:
+            if picked_up_first_row[i] in stats["minecraft:picked_up"]:
+                picked_up_worksheet.write(row, i + 1, stats["minecraft:picked_up"][picked_up_first_row[i]], cell_stat)
+    for i in range(0, len(custom_first_row)):
+        custom_worksheet.write_blank(row, i + 1, "", cell_stat)
+        if "minecraft:custom" in stats:
+            if custom_first_row[i] in stats["minecraft:custom"]:
+                if custom_first_row[i] == "minecraft:play_one_minute":
+                    custom_worksheet.write(row, i + 1, stats["minecraft:custom"][custom_first_row[i]] / (20.0 * 60 * 60), cell_stat)
+                    custom_worksheet.write(0, i + 1, "time played in hours".title(), cell_first_row)
+                elif "time" in custom_first_row[i]:
+                    custom_worksheet.write(row, i + 1, stats["minecraft:custom"][custom_first_row[i]] / (20.0 * 60), cell_stat)
+                elif "one_cm" in custom_first_row[i]:
+                    custom_worksheet.write(row, i + 1, stats["minecraft:custom"][custom_first_row[i]] / (100.0 * 1000), cell_stat)
+                    custom_worksheet.write(0, i + 1, (custom_first_row[i][10:-2:] + "km").replace("_", " ").title(), cell_first_column)
+                else:
+                    custom_worksheet.write(row, i + 1, stats["minecraft:custom"][custom_first_row[i]], cell_stat)
+    for i in range(0, len(broken_first_row)):
+        broken_worksheet.write_blank(row, i + 1, "", cell_stat)
+        if "minecraft:broken" in stats:
+            if broken_first_row[i] in stats["minecraft:broken"]:
+                broken_worksheet.write(row, i + 1, stats["minecraft:broken"][broken_first_row[i]], cell_stat)
+    row += 1
+
+# Freeze first column
+for sheet in worksheets:
+    sheet.freeze_panes(0, 1)
+
+# Defining outer ranges
+killed_by_outer_range = column_string(len(killed_by_first_row) + 1) + str(len(players) + 1)
+crafted_outer_range = column_string(len(crafted_first_row) + 1) + str(len(players) + 1)
+mined_outer_range = column_string(len(mined_first_row) + 1) + str(len(players) + 1)
+used_outer_range = column_string(len(used_first_row) + 1) + str(len(players) + 1)
+killed_outer_range = column_string(len(killed_first_row) + 1) + str(len(players) + 1)
+dropped_outer_range = column_string(len(dropped_first_row) + 1) + str(len(players) + 1)
+picked_up_outer_range = column_string(len(picked_up_first_row) + 1) + str(len(players) + 1)
+custom_outer_range = column_string(len(custom_first_row) + 1) + str(len(players) + 1)
+broken_outer_range = column_string(len(broken_first_row) + 1) + str(len(players) + 1)
+
+# Custom conditional formatting: =B2=MAX(B$2:B$12) and alternating colors
+killed_by_worksheet.conditional_format("B2:" + killed_by_outer_range, {"type": "formula", "criteria": "=B2=MAX(B$2:B$" + str(len(players) + 1) + ")", "format": cell_light_magenta})
+killed_by_worksheet.conditional_format("B2:" + killed_by_outer_range, {"type": "formula", "criteria": "=MOD(ROW(),2)=0", "format": cell_light_pink})
+crafted_worksheet.conditional_format("B2:" + crafted_outer_range, {"type": "formula", "criteria": "=B2=MAX(B$2:B$" + str(len(players) + 1) + ")", "format": cell_light_magenta})
+crafted_worksheet.conditional_format("B2:" + crafted_outer_range, {"type": "formula", "criteria": "=MOD(ROW(),2)=0", "format": cell_light_pink})
+mined_worksheet.conditional_format("B2:" + mined_outer_range, {"type": "formula", "criteria": "=B2=MAX(B$2:B$" + str(len(players) + 1) + ")", "format": cell_light_magenta})
+mined_worksheet.conditional_format("B2:" + mined_outer_range, {"type": "formula", "criteria": "=MOD(ROW(),2)=0", "format": cell_light_pink})
+used_worksheet.conditional_format("B2:" + used_outer_range, {"type": "formula", "criteria": "=B2=MAX(B$2:B$" + str(len(players) + 1) + ")", "format": cell_light_magenta})
+used_worksheet.conditional_format("B2:" + used_outer_range, {"type": "formula", "criteria": "=MOD(ROW(),2)=0", "format": cell_light_pink})
+killed_worksheet.conditional_format("B2:" + killed_by_outer_range, {"type": "formula", "criteria": "=B2=MAX(B$2:B$" + str(len(players) + 1) + ")", "format": cell_light_magenta})
+killed_worksheet.conditional_format("B2:" + killed_by_outer_range, {"type": "formula", "criteria": "=MOD(ROW(),2)=0", "format": cell_light_pink})
+dropped_worksheet.conditional_format("B2:" + dropped_outer_range, {"type": "formula", "criteria": "=B2=MAX(B$2:B$" + str(len(players) + 1) + ")", "format": cell_light_magenta})
+dropped_worksheet.conditional_format("B2:" + dropped_outer_range, {"type": "formula", "criteria": "=MOD(ROW(),2)=0", "format": cell_light_pink})
+picked_up_worksheet.conditional_format("B2:" + picked_up_outer_range, {"type": "formula", "criteria": "=B2=MAX(B$2:B$" + str(len(players) + 1) + ")", "format": cell_light_magenta})
+picked_up_worksheet.conditional_format("B2:" + picked_up_outer_range, {"type": "formula", "criteria": "=MOD(ROW(),2)=0", "format": cell_light_pink})
+custom_worksheet.conditional_format("B2:" + custom_outer_range, {"type": "formula", "criteria": "=B2=MAX(B$2:B$" + str(len(players) + 1) + ")", "format": cell_light_magenta})
+custom_worksheet.conditional_format("B2:" + custom_outer_range, {"type": "formula", "criteria": "=MOD(ROW(),2)=0", "format": cell_light_pink})
+broken_worksheet.conditional_format("B2:" + broken_outer_range, {"type": "formula", "criteria": "=B2=MAX(B$2:B$" + str(len(players) + 1) + ")", "format": cell_light_magenta})
+broken_worksheet.conditional_format("B2:" + broken_outer_range, {"type": "formula", "criteria": "=MOD(ROW(),2)=0", "format": cell_light_pink})
+
+# Adding autofilters
+killed_by_worksheet.autofilter("B1:" + killed_by_outer_range)
+crafted_worksheet.autofilter("B1:" + crafted_outer_range)
+mined_worksheet.autofilter("B1:" + mined_outer_range)
+used_worksheet.autofilter("B1:" + used_outer_range)
+killed_worksheet.autofilter("B1:" + killed_outer_range)
+dropped_worksheet.autofilter("B1:" + dropped_outer_range)
+picked_up_worksheet.autofilter("B1:" + picked_up_outer_range)
+custom_worksheet.autofilter("B1:" + custom_outer_range)
+broken_worksheet.autofilter("B1:" + broken_outer_range)
+
+workbook.close()
